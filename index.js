f37ec0f347b23ee8fbb803d6fc22fa9214f67dd1
WeiMeng03 <meng.wei03@ele.me>
2019-06-11 18:40:47 +0800

    modify

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-import express from 'express'
-import path from 'path'
-import fs from 'fs'
-
+const express = require('express')
+const path = require('path')
 const app = express()
 
 app.get('*', (req, res) => {
-  const path = req.path
   let file = ''
-  if(path === '' || path === '/' ) {
+  if(req.path === '' || req.path === '/' ) {
     file = path.resolve(__dirname, 'html/index.html')
   } else {
     file = path.resolve(__dirname, 'html/' + req.path + '.html')
